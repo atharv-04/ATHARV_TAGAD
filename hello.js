e5bdaadda2fd59c69401869fe38e36e1d37055fd
Atharv Tagad <atharvtagad99@gmail.com>
2025-06-05 09:27:56 +0530

    first commit

---
+++ hello.js
@@ -0,0 +1,15 @@
+console.log("hello world!");
+let val=19;
+val='hello';
+val=false;
+val='world';
+console.log(val)
+
+let num=4;
+if(num%2==0)
+{
+    console.log("the number is even")
+} else 
+{
+    console.log("the number is odd")
+}
